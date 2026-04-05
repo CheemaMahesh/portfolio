4bfc8fb58d0b44c0181b17c5292c2efe6ccf756c
CheemaMahesh <maheshbabucheema789@gmail.com>
2026-04-05 23:41:33 +0530

    fix (motion.p): Can not keep section inside p tag

--- src/features/experience/components/details-card.tsx
+++ src/features/experience/components/details-card.tsx
@@ -59,7 +59,7 @@ export const DetailsCard = ({
         {title}
       </motion.h3>
 
-      <motion.p
+      <motion.div
         initial={{ opacity: 0, y: 20 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
@@ -79,7 +79,7 @@ export const DetailsCard = ({
             />
           </Link>
         </section>
-      </motion.p>
+      </motion.div>
 
       <motion.ul
         initial={{ opacity: 0 }}
